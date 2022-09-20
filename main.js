51b8a83ee15d23ed496e8f654d3ea8630bf1830a
kimsho9803 <kimsho98@naver.com>
2022-09-20 18:52:25 +0900

    added funcs

--- main.js
+++ main.js
@@ -6,8 +6,17 @@ function add(a, b) {
     return a + b;
 }
 
+<<<<<<< HEAD
 function mod(a, b) {
     return a % b;
+=======
+function sub(a, b) {
+
+}
+
+function mul(a, b) {
+
+>>>>>>> a869cff (added funcs)
 }
 
 console.log(div(10, 5))
