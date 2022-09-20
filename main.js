c7beec60165c6129949de7b9d5a34dd4ba7b0876
kimsho9803 <kimsho98@naver.com>
2022-09-20 19:10:29 +0900

    finish week 5

--- main.js
+++ main.js
@@ -6,21 +6,16 @@ function add(a, b) {
     return a + b;
 }
 
-<<<<<<< HEAD
 function mod(a, b) {
     return a % b;
-=======
+}
+
 function sub(a, b) {
     return a - b;
 }
 
 function mul(a, b) {
-<<<<<<< HEAD
-
->>>>>>> a869cff (added funcs)
-=======
     return a * b;
->>>>>>> b4abe6e (added mul func)
 }
 
 console.log(div(10, 5))
